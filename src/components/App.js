4b8e6c05299cc1b8ffd9865458adca850c1c1c10
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-05-17 03:06:01 -0300

    Clean app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,9 @@
-import { useState } from 'react';
-import useFetch from '../hooks/useFetch';
-import {useDisclosure} from '@chakra-ui/react'
-import pokeApi from '../pokeApi';
 import Menu from './Menu';
-import Pokemon from './Pokemon';
 
 function App() {
-  const [numeroPagina, setPagina] = useState(0);
-  const [numeroPokemon, setPokemon] = useState();
-  const listaPokemon = useFetch(pokeApi.nombre, numeroPokemon)
-  const dataPokemon = useFetch(pokeApi.lista, numeroPagina)
- 
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
     <div className="App">
-      <Menu onOpen={onOpen} lista={listaPokemon}/>
-      <Pokemon isOpen={isOpen} onClose={onClose} datos={dataPokemon}/>
+      <Menu />
     </div>
   );
 }
