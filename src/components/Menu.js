27339185340bd35d18f6db8e594ec56a8ba67f45
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-05-17 03:06:37 -0300

    Add List functionality

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,38 @@
+import { useState } from 'react';
+import useFetch from '../hooks/useFetch';
+import pokeApi from '../pokeApi';
 import Item from "./Item";
-import { Stack } from '@chakra-ui/react'
+import { Stack, Spinner,Grid, GridItem } from '@chakra-ui/react'
 
-function Menu({onOpen}) {
+
+function Menu() {
+  const [pagina, setPagina] = useState('https://pokeapi.co/api/v2/pokemon/');
+  const [dataPagina] = useFetch(pokeApi.getPokemonListByPage, pagina);
+  const [pokemon, setPokemon] = useState('ninguno');
+  const [dataPokemon] = useFetch(pokeApi.getPokemonListByPage, pokemon);
+  console.log(dataPagina,dataPokemon);
+  
   return (
-    <Stack spacing={4} direction='row' align='center'>
-      <Item onOpen={onOpen}/>
-    </Stack>
-  );
+    <>
+      <Item name='<<' onClick={() => {setPagina(dataPagina.previous)}}/>
+        <Grid templateColumns='repeat(20, 1fr)' gap={6}>
+      {dataPagina?.results.map(({name,url}) => (
+        <GridItem w='32' h='12' bg='blue.500' >
+          <Item 
+            key={name}
+            name={name}
+            number={url.split('/')[6]} 
+            onClick={() => {
+              setPokemon(url);
+            }}
+          />
+        </GridItem>
+      )) ?? <Spinner />
+      }
+
+    </Grid>
+      <Item name='>>' onClick={() => {setPagina(dataPagina.next)}}/>
+  </>);
 }
 
 export default Menu;
