bbb3478bc75614e72e38413773168db6d86caea3
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-05-18 07:30:23 -0300

    Feat Pokemon showing modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+import useFetch from '../hooks/useFetch';
+import pokeApi from '../pokeApi';
 import Menu from './Menu';
+import { Container, useDisclosure } from '@chakra-ui/react'
+import Pokemon from './Pokemon';
 
 function App() {
+  const [pokemon, setPokemon] = useState('ninguno');
+  const [dataPokemon] = useFetch(pokeApi.getPokemonListByPage, pokemon);
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+
   return (
-    <div className="App">
-      <Menu />
-    </div>
+    <Container m={2} w='100%'>
+      <Menu setPokemon={setPokemon} onOpen={onOpen}/>
+      <Pokemon {...dataPokemon} isOpen={isOpen} onClose={onClose}/>
+    </Container>
   );
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,37 +2,45 @@ import { useState } from 'react';
 import useFetch from '../hooks/useFetch';
 import pokeApi from '../pokeApi';
 import Item from "./Item";
-import { Stack, Spinner,Grid, GridItem } from '@chakra-ui/react'
+import { Spinner,Grid, GridItem } from '@chakra-ui/react'
 
 
-function Menu() {
+function Menu({setPokemon, onOpen}) {
   const [pagina, setPagina] = useState('https://pokeapi.co/api/v2/pokemon/');
   const [dataPagina] = useFetch(pokeApi.getPokemonListByPage, pagina);
-  const [pokemon, setPokemon] = useState('ninguno');
-  const [dataPokemon] = useFetch(pokeApi.getPokemonListByPage, pokemon);
-  console.log(dataPagina,dataPokemon);
+
+
   
   return (
-    <>
-      <Item name='<<' onClick={() => {setPagina(dataPagina.previous)}}/>
-        <Grid templateColumns='repeat(20, 1fr)' gap={6}>
-      {dataPagina?.results.map(({name,url}) => (
-        <GridItem w='32' h='12' bg='blue.500' >
-          <Item 
-            key={name}
-            name={name}
-            number={url.split('/')[6]} 
-            onClick={() => {
-              setPokemon(url);
-            }}
-          />
-        </GridItem>
-      )) ?? <Spinner />
-      }
-
+    <Grid  
+      h='200px'
+      templateRows='repeat(2, 1fr)'
+      templateColumns='repeat(11, 1fr)'
+      gap={6}
+      bg='blue.300'
+    >
+      <GridItem rowSpan={2} bg='blue.500' >
+        <Item name='<<' onClick={() => {setPagina(dataPagina.previous)}}/>
+      </GridItem>
+    {dataPagina?.results.map(({name,url}) => (
+      <GridItem bg='blue.500' w='100%' >
+        <Item 
+          key={name}
+          name={name}
+          number={url.split('/')[6]} 
+          onClick={() => {
+            setPokemon(url);
+            onOpen();
+          }}
+        />
+      </GridItem>
+      )) ?? <Spinner /> 
+    }
+      <GridItem rowSpan={2} bg='blue.500' >
+        <Item name='>>' onClick={() => {setPagina(dataPagina.next)}}/>
+      </GridItem>
     </Grid>
-      <Item name='>>' onClick={() => {setPagina(dataPagina.next)}}/>
-  </>);
+  );
 }
 
 export default Menu;
