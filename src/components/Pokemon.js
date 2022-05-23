8fc127cf4fde768bcc836bfdc6f5214f1d3b77b9
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-05-23 02:14:21 -0300

    Update Pokemon.js

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,15 +1,24 @@
 import {
-  SimpleGrid,  
+  Flex,
   Text,
   Image,
 } from  '@chakra-ui/react'
 
-function Pokemon({forms, sprite}) {
+function Pokemon({nameStat, sprite, types, id}) {
+  const textColor = 'white';
   return (
-    <SimpleGrid h='400px' w='250px' m={3} justifyContent={'center'} bg={'cyan.100'}>       
-        <Text textAlign={'center'}>{forms?.[0].name.toUpperCase() ?? 'Selecciona un Pokemon'}</Text>
-        <Image src={sprite?.other['official-artwork'].front_default ?? '../../img/pokebola.png' } alt='Pokemon image' />
-    </SimpleGrid>
+    <Flex h='400px' w='250px' m={3} rounded={'sm'} bg={'red.700'} alignContent={'center'} flexDirection={'column'} justifyContent={'space-between'}>       
+        <Text textAlign={'center'} m={3} fontSize='xl' fontWeight="bold" color={textColor}>
+          {nameStat?.toUpperCase() ?? 'Selecciona un Pokemon'}
+        </Text>
+        <Text textAlign={'center'} fontSize='lg' fontWeight="bold" color={textColor}>
+          {id ? '# ' + id : ''}
+        </Text>       
+        <Text textAlign={'center'} fontSize='lg' fontWeight="bold" color={textColor}>
+          {types ? 'TYPE: ' + types.map((e) => e.toUpperCase()) : ''}
+        </Text>
+        <Image bg={'gray.600'} m={3} borderColor={'black'} border={'2px'} rounded={'xl'} src={sprite?.other['official-artwork'].front_default ?? '../../img/pokebola.png' } alt='Pokemon image' />
+    </Flex>
   )
 }
 
