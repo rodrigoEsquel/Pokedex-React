734f353d54785d0748054dbe08fde2a5dfea8cf2
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-05-23 02:14:08 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, Heading } from '@chakra-ui/react'
 
 function Header() {
   return (  
-    <Flex h='62px' justifyContent={'center'} bg={'red.500'}  grow='0' alignItems={'center'}>
-      <Text bg="#ff0"
+    <Flex h='62px' justifyContent={'center'} bg={'red.500'} alignItems={'center'}>
+      <Heading  bg="yellow.300"
         bgClip="text"
         fontSize="3xl"
         fontWeight="extrabold"
         textAlign='center'>
         Poke-API
-      </Text>
+      </Heading >
     </Flex>
   );
 }
