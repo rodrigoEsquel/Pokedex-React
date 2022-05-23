7da4d9547015122d0638d894384dc8b83f145d85
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-05-23 02:14:04 -0300

    Update Display.js

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,20 @@
 import { Text, Flex } from '@chakra-ui/react'
 
-function Display() {
+function Display({heightStat, weightStat, stats}) {
+  const textColor = 'white';
   return (
-    <Flex m={3}>
-      <Text>Display</Text>
+    <Flex bg={'red.700'} flexDirection={'column'} justifyContent='space-evenly' grow='1'>  
+      <Text textAlign={'center'} fontSize='lg' fontWeight="bold" color={textColor}>    
+        {heightStat ? 'HEIGHT: ' + heightStat : ''}
+      </Text>
+      <Text textAlign={'center'} fontSize='lg' fontWeight="bold" color={textColor}>
+        {weightStat ? 'WEIGHT: ' + weightStat : ''}
+      </Text>   
+      {stats?.map((stat) => (
+        <Text textAlign={'center'} fontSize='lg' fontWeight="bold" color={textColor} key={stat.name}>    
+          {stat.name.toUpperCase() + ': ' + stat.value }
+        </Text>
+      )) ?? ''}
     </Flex>
   )
 
