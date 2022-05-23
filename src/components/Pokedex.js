8f68c34269feb0c26e02a894278f2cfa74239bd8
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-05-23 02:14:15 -0300

    Update Pokedex.js

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,11 +1,11 @@
 import { useState, useRef } from 'react';
-import { Flex, SimpleGrid } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 import useFetch from '../hooks/useFetch';
+import pokeApi, { baseUrl } from '../pokeApi';
 import Menu from './Menu';
 import Pokemon from './Pokemon';
 import Display from './Display'
 import ButtonPad from './ButtonPad';
-import pokeApi, { baseUrl } from '../pokeApi';
 
 function Pokedex() {
   const [pagina, setPagina] = useState(baseUrl);
@@ -13,18 +13,16 @@ function Pokedex() {
   const refInputValue = useRef('');
   const [pokemon, setPokemon] = useState(null);
   const [dataPokemon] = useFetch(pokeApi.getPokemonListByPage, pokemon);  
-  console.log(dataPokemon);
 
   return (
-
     <Flex justifyContent={'center'} alignItems={'center'} grow='1' >
-      <Flex flexDirection={{sm: 'column', lg: 'row'}} m={5} bg={'red.500'} justifyContent={'space-evenly'} alignItems={'center'} rounded={'md'}>
+      <Flex m={5} p={3} bg={'red.500'} rounded={'md'} flexDirection={{sm: 'column', lg: 'row'}} justifyContent={'space-evenly'} alignItems={'center'}>
       <Flex flexDirection={{sm: 'column', md: 'row'}} justifyContent={'space-evenly'}>
-        <Pokemon sprite={dataPokemon?.sprites} forms={dataPokemon?.forms}/>
-        <SimpleGrid columns={1} h='400px' w='250px' my={{sm:0, md:3}} mx={{sm:3, md:3}} justifyContent={'center'} bg={'red.600'}>
-          <Display />
+        <Pokemon sprite={dataPokemon?.sprites} nameStat={dataPokemon?.name} types={dataPokemon?.types.map((pkmn) => pkmn.type.name)} id={dataPokemon?.id}/>
+        <Flex flexDirection={'column'} h='400px' w='250px' m={3} justifyContent={'space-between'}>
+          <Display heightStat={dataPokemon?.height} weightStat={dataPokemon?.weight} stats={dataPokemon?.stats.map((pkmn)=>({name: pkmn.stat.name, value: pkmn.base_stat}))}/>
           <ButtonPad previousPage={dataPagina?.previous} nextPage={dataPagina?.next} inputValue={refInputValue.current} setPagina={setPagina} setPokemon={setPokemon}/>
-        </SimpleGrid>
+        </Flex>
       </Flex >
       <Menu setPokemon={setPokemon} listResults={dataPagina?.results} /> 
     </Flex>
